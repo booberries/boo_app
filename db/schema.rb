4fe4d26da9de0f0ccb07494d43a41fd90cb3f482
Caleb Horton <caleb.a.horton@gmail.com>
2019-04-15 18:58:35 -0500

    Added articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_225353) do
+ActiveRecord::Schema.define(version: 2019_04_15_233936) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "static_pages", force: :cascade do |t|
     t.datetime "created_at", null: false
